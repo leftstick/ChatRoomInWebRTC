d4b622963ad20497408ca9a31247a37997295dc9
zuohao <leftstick@qq.com>
2014-01-15 13:15:45 +0800

    some updates

--- src/js/ChatService.js
+++ src/js/ChatService.js
@@ -16,3 +16,22 @@ Chat.factory('Data', [
         return factory;
    }]
 );
+
+/**
+ * Service, PeerJS wapper.
+ */
+Chat.factory('PeerJS', [
+    function() {
+        var factory = {};
+        var peer;
+        var connection = [];
+        factory.createPeer = function(){
+            peer = new Peer('leftstick-unique', {key: 'm4lam1d6op28d7vi'});
+            peer.on('connection', function(conn) {
+                
+            });
+            return peer;
+        };
+        return factory;
+   }]
+);
